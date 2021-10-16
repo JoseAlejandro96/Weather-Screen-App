3f2f9795e224dc4b8f8bfdcff2ec5868faba79c6
JoseAlejandro96 <jscuesta96@gmail.com>
2021-10-16 22:03:42 +0200

    finished

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Type.kt
@@ -0,0 +1,22 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.R
+
+
+val CustomFont = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_medium, FontWeight.Medium)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = CustomFont
+)
